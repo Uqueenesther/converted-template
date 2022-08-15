e884eda332c8e2938ff6bb24db5ac15640a823c7
Queenesther <umohqueenesther@gmail.com>
2022-08-15 15:51:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Home from "./pages/Home"
+import { Routes, Route, Link } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+ 
